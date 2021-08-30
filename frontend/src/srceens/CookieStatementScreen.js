add258ce836d37e489f47e28b79ee06f2c342980
henk687 <henkleenheer@upcmail.nl>
2021-08-30 15:13:05 +0200

    changes

--- frontend/src/srceens/CookieStatementScreen.js
+++ frontend/src/srceens/CookieStatementScreen.js
@@ -6,8 +6,8 @@ const CookieStatementScreen = {
         <h2>Wat is een cookie?</h2>
         <p>
           Een cookie is een klein tekstbestand dat bij een bezoek aan een website op je computer wordt geplaatst maar ze kunnen geen virussen verspreiden. 
-          Een cookie is uniek en kan alleen worden gelezen door de server die je de cookie heeft verschaft. Zowel Firefly Webdesign als andere partijen kunnen een cookie plaatsen. 
-          Waarom maakt Firefly Webdesign gebruik van cookies? Firefly Webdesign maakt gebruik van cookies om je een zo’n goed mogelijke gebruikerservaring aan te bieden. 
+          Een cookie is uniek en kan alleen worden gelezen door de server die je de cookie heeft verschaft. Zowel Hobby Verkoop als andere partijen kunnen een cookie plaatsen. 
+          Waarom maakt Hobby Verkoop gebruik van cookies? Hobby Verkoop maakt gebruik van cookies om je een zo’n goed mogelijke gebruikerservaring aan te bieden. 
           Dankzij cookies kunnen gegevens worden bewaard en hoef je niet elke keer je gegevens in te vullen. Kortom, cookies helpen je tijd te besparen.
           <br />Daarnaast maken we gebruik van Google Analytics om het gebruiksgemak op de langere termijn te verbeteren. 
           De cookies die voor Google Analytics worden geplaatst slaan geen persoonlijke data op van bezoekers.&nbsp; Voor meer informatie over cookies en 
@@ -15,11 +15,11 @@ const CookieStatementScreen = {
         </p>
         <h2>Hoe kan ik me afmelden voor cookies?</h2>
         <p>
-          Mocht je liever niet hebben dat er cookies tijdens je bezoek aan Firefly Webdesign worden geplaatst dan kunt je je internetbrowser zo instellen dat deze geen cookies opslaat. 
+          Mocht je liever niet hebben dat er cookies tijdens je bezoek aan Hobby Verkoop worden geplaatst dan kunt je je internetbrowser zo instellen dat deze geen cookies opslaat. 
           Voor de specifieke instellingen verwijzen wij je graag naar de handleiding van je browser, meestal te vinden onder ‘Help’ in de toolbar van de meeste browsers.
         </p>
         <h1>Contact</h1>
-        <p>Heb je vragen over cookies van Firefly Webdesign dan kun je contact opnemen via <a href="mailto:info@fireflywebdesign.nl."></p>
+        <p>Heb je vragen over cookies van Hobby Verkoop dan kun je contact opnemen via <a href="mailto:info@hobbyverkoop.com">info@hobbyverkoop.com</a></p>
       </div>`;
   },
 };
